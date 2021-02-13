a20a783265d009d15f7822530ddaa6bc4a0bc4fc
Favian Flores <floresf1997@gmail.com>
2021-02-13 00:42:32 -0500

    added 2 features and modified settings page

--- Tipper/MainViewController.swift
+++ Tipper/MainViewController.swift
@@ -1,65 +1,101 @@
-//
 //  MainViewController.swift
 //  Tipper
 //
 //  Created by Favian Flores on 1/30/21.
-//
 
 import UIKit
 
 class MainViewController: UIViewController {
-
     
+	// Outlets
     @IBOutlet weak var billAmountTextField: UITextField!
-    @IBOutlet weak var tipAmountLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+	@IBOutlet weak var numberOfPeopleTextField: UITextField!
+	@IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    
+	@IBOutlet weak var tipController: UISegmentedControl!
+	
+	// Global
+	let defaults = UserDefaults.standard
+	
+	// User Inputs
+	@IBAction func textFieldInput(_ sender: Any) {
+		calculateTip()
+	}
+	
+	@IBAction func segmentedControlInput(_ sender: Any) {
+		calculateTip()
+	}
+	 
+	// View Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        let defaults = UserDefaults.standard
-        if defaults.value(forKey: "left") == nil {
-            defaults.set(15, forKey: "left")
-        }
-        if defaults.value(forKey: "mid") == nil {
-            defaults.set(18, forKey: "mid")
-        }
-        if defaults.value(forKey: "right") == nil {
-            defaults.set(20, forKey: "right")
-        }
-        billAmountTextField.becomeFirstResponder()
-    }
-    
-    func calculateTip() {
-        let bill = Double(billAmountTextField.text!) ?? 0.0
-        let defaults = UserDefaults.standard
-        let left = defaults.integer(forKey: "left")
-        let mid = defaults.integer(forKey: "mid")
-        let right = defaults.integer(forKey: "right")
-        let tipPercentages = [left, mid, right]
-        let tip = bill * Double(tipPercentages[tipControl.selectedSegmentIndex]) * 0.01
-        let total = bill + tip
-        
-        tipAmountLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-    }
-    
-    @IBAction func textFieldInput(_ sender: Any) {
-        calculateTip()
-    }
-    
-    @IBAction func segmentedControlInput(_ sender: Any) {
-        calculateTip()
+		NotificationCenter.default.addObserver(self, selector: #selector(billAmountTimer), name: UIApplication.willResignActiveNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(restoreBillAmount), name: UIApplication.willEnterForegroundNotification, object: nil)
+		restoreBillAmount()
+		setTipControllerDefaults()
+		billAmountTextField.becomeFirstResponder()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        let defaults = UserDefaults.standard
-        tipControl.setTitle(String(defaults.integer(forKey: "left")) + "%", forSegmentAt: 0)
-        tipControl.setTitle(String(defaults.integer(forKey: "mid")) + "%", forSegmentAt: 1)
-        tipControl.setTitle(String(defaults.integer(forKey: "right")) + "%", forSegmentAt: 2)
+        updateTipControllerValues()
         billAmountTextField.becomeFirstResponder()
         calculateTip()
     }
-}
+	
+	deinit {
+		NotificationCenter.default.removeObserver(self)
+	}
+	
+	// Functions
+	func setTipControllerDefaults() {
+		if defaults.value(forKey: "tipControllerLeft") == nil {
+			defaults.set(15, forKey: "tipControllerLeft")
+		}
+		if defaults.value(forKey: "tipControllerMiddle") == nil {
+			defaults.set(18, forKey: "tipControllerMiddle")
+		}
+		if defaults.value(forKey: "tipControllerRight") == nil {
+			defaults.set(20, forKey: "tipControllerRight")
+		}
+		if defaults.value(forKey: "tipControllerChoice") == nil {
+			defaults.set(1, forKey: "tipControllerChoice")
+		}
+	}
+	
+	func updateTipControllerValues() {
+		tipController.setTitle(String(defaults.integer(forKey: "tipControllerLeft")) + "%", forSegmentAt: 0)
+		tipController.setTitle(String(defaults.integer(forKey: "tipControllerMiddle")) + "%", forSegmentAt: 1)
+		tipController.setTitle(String(defaults.integer(forKey: "tipControllerRight")) + "%", forSegmentAt: 2)
+		tipController.selectedSegmentIndex = defaults.integer(forKey: "tipControllerChoice")
+	}
 
+	func calculateTip() {
+		let left = defaults.integer(forKey: "tipControllerLeft")
+		let mid = defaults.integer(forKey: "tipControllerMiddle")
+		let right = defaults.integer(forKey: "tipControllerRight")
+		let tipPercentages = [left, mid, right]
+		
+		let bill = Double(billAmountTextField.text!) ?? 0.0
+		let tip = bill * Double(tipPercentages[tipController.selectedSegmentIndex]) * 0.01
+		let total = bill + tip
+		
+		tipAmountLabel.text = String(format: "$%.2f", tip)
+		totalLabel.text = String(format: "$%.2f", total)
+		
+		defaults.set(Double(billAmountTextField.text!), forKey: "billAmount")
+		defaults.set(Int(tipController.selectedSegmentIndex), forKey: "tipControllerIndex")
+	}
+
+	@objc func restoreBillAmount() {
+		let date = Date()
+		if (defaults.value(forKey: "deleteBillAmountBy") != nil && date > defaults.value(forKey: "deleteBillAmountBy") as! Date) {
+			defaults.set("", forKey: "billAmount")
+		}
+		billAmountTextField.text = defaults.string(forKey: "billAmount")
+	}
+	
+	@objc func billAmountTimer() {
+		defaults.set(Date(timeInterval: 600, since: Date()), forKey: "deleteBillAmountBy")
+	}
+}
--- Tipper/SettingsViewController.swift
+++ Tipper/SettingsViewController.swift
@@ -1,33 +1,77 @@
-//
 //  SettingsViewController.swift
 //  Tipper
 //
 //  Created by Favian Flores on 2/1/21.
-//
 
 import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var TipControllerLeft: UITextField!
-    @IBOutlet weak var TipControllerMid: UITextField!
-    @IBOutlet weak var TipControllerRight: UITextField!
-    
+	// Outlets
+    @IBOutlet weak var tipControllerLeft: UITextField!
+    @IBOutlet weak var tipControllerMiddle: UITextField!
+    @IBOutlet weak var tipControllerRight: UITextField!
+	@IBOutlet weak var sampleTipController: UISegmentedControl!
+	
+	let defaults = UserDefaults.standard
+	
+	// View Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        TipControllerLeft.becomeFirstResponder()
+        tipControllerLeft.becomeFirstResponder()
     }
+	
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        let defaults = UserDefaults.standard
-        if TipControllerLeft.text! != "" {
-            defaults.set(Double(TipControllerLeft.text!), forKey: "left")
-        }
-        if TipControllerMid.text! != "" {
-            defaults.set(Double(TipControllerMid.text!), forKey: "mid")
-        }
-        if TipControllerRight.text! != "" {
-            defaults.set(Double(TipControllerRight.text!), forKey: "right")
-        }
+		saveTipControllerValues()
     }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		sampleTipController.setTitle(String(defaults.integer(forKey: "tipControllerLeft")) + "%", forSegmentAt: 0)
+		sampleTipController.setTitle(String(defaults.integer(forKey: "tipControllerMiddle")) + "%", forSegmentAt: 1)
+		sampleTipController.setTitle(String(defaults.integer(forKey: "tipControllerRight")) + "%", forSegmentAt: 2)
+		sampleTipController.selectedSegmentIndex = defaults.integer(forKey: "tipControllerChoice")
+	}
+	
+	// Functions
+	func saveTipControllerValues() {
+		if tipControllerLeft.text! != "" {
+			defaults.set(Double(tipControllerLeft.text!), forKey: "tipControllerLeft")
+		}
+		if tipControllerMiddle.text! != "" {
+			defaults.set(Double(tipControllerMiddle.text!), forKey: "tipControllerMiddle")
+		}
+		if tipControllerRight.text! != "" {
+			defaults.set(Double(tipControllerRight.text!), forKey: "tipControllerRight")
+		}
+		defaults.set(Int(sampleTipController.selectedSegmentIndex), forKey: "tipControllerChoice")
+	}
+	
+	@IBAction func updateSampleTipController(_ sender: Any) {
+		var left : String, middle : String, right : String
+		if tipControllerLeft.text! == "" {
+			left = defaults.string(forKey: "tipControllerLeft")! + "%"
+		}
+		else {
+			left = tipControllerLeft.text! + "%"
+		}
+		if tipControllerMiddle.text! == "" {
+			middle = defaults.string(forKey: "tipControllerMiddle")! + "%"
+		}
+		else {
+			middle = tipControllerMiddle.text! + "%"
+		}
+		if tipControllerRight.text! == "" {
+			right = defaults.string(forKey: "tipControllerRight")! + "%"
+		}
+		else {
+			right = tipControllerRight.text! + "%"
+		}
+		sampleTipController.setTitle(left, forSegmentAt: 0)
+		sampleTipController.setTitle(middle, forSegmentAt: 1)
+		sampleTipController.setTitle(right, forSegmentAt: 2)
+	}
+	
+	
 }
